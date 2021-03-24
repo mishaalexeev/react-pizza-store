0c5c2762c907afb26c50d4b71610f153983eda5d
Misha Alexeev <meeshaalexeev@gmail.com>
2021-03-24 18:17:08 +0300

    5 урок, pizzablock, axios/fetch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // тут собираются все отдельные компоненты и отправляются в <App/> в index.js
 
 import React from 'react';
+
 import { Header} from './сomponents';  // первым делом ищет index.js
 //import SortPopup from './сomponents/SortPopup';
 // import Header from './сomponents'; // формат указывать не обязательно
 import  { Home, Cart } from './pages';
 import {Route} from 'react-router-dom';
+
+import axios from 'axios';
 
 //function Button() { // не обычная функция, а функциональный компонент
 //  return <button>Добавить</button>  // функция возвращает HTML
@@ -16,6 +19,23 @@ function App() { // функциональный компонент, HTML пре
  //   alert('Hello!');
  // }
 
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => { // будет выполняться единожды при первом рендере --- js fetch, fetch возвращает promise (получение ассинхронных данных, в данном случае db.json)
+    axios.getUri('http://localhost:3000/db.json').then(({data}) => {  // ------------------------------ axios для более крупных проектов
+      setPizzas(data.pizzas);
+    }
+    )
+
+
+    // fetch('http://localhost:3000/db.json') --------------- fetch
+    // .then((resp) => resp.json())
+    // .then((json) => {
+    //   setPizzas(json.pizzas);
+    // });
+  },[]);
+
+
   return ( // тут используется JSX, в JS можно писать HTML код
     //React.createElement('div', null, 'Hello World') - можно так
     //<div><h1>Hello World</h1></div> - а с JSX можно так
@@ -23,7 +43,8 @@ function App() { // функциональный компонент, HTML пре
       <Header/>
 
       <div className="content">
-        <Route path="/" component={Home} exact/>
+        <Route path="/" render={() => <Home items={pizzas}/>} exact/> 
+        {/* если чето отрендерить то надо рендер писать */}
         <Route path="/cart" component={Cart} exact/>
       </div>
     </div>
